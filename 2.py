039036718556dfd2f68ed5266d9110caf8fbd13e
darwin <dturpoqu@unsa.edu.pe>
2025-08-17 20:04:37 -0500

    2 Ending

---
+++ 2.py
@@ -0,0 +1,11 @@
+# Crea una variable para guardar el nombre de tu película o serie favorita.
+
+# Crea otra variable para guardar el año en que se estrenó.
+
+# Muestra en pantalla un mensaje que diga: "Mi serie/película favorita es
+# [Nombre de la película/serie] y se estrenó en el año [Año de estreno].", 
+# usando las variables que creaste.
+
+nMovie = "Stranger Things"
+dPremier = '2/9/2019'
+print(f"My favorite series is {nMovie} and it premier was in {dPremier}.")
