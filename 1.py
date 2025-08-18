d0618282db3f936271343aa6a7d164cdaf6b407c
darwin <dturpoqu@unsa.edu.pe>
2025-08-17 19:55:49 -0500

    Completo ejem1

--- 1.py
+++ 1.py
@@ -1,1 +1,5 @@
-print("Hola, mundo!")
+#Escribe un programa que muestre en la pantalla tu nombre completo y el país desde
+#donde estás aprendiendo a programar. Cada dato debe estar en una línea separada.
+
+print("Darwin Turpo")
+print("Peru")
