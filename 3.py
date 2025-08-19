0cb09d044c1ce05d85392c8e804070417ee36482
darwin <dturpoqu@unsa.edu.pe>
2025-08-18 20:53:21 -0500

    dia2

---
+++ 3.py
@@ -0,0 +1,10 @@
+num1 = 22
+num2 = 44
+sum = num1 + num2
+res = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print(f' la suma de {num1} y {num2} es : {sum}')
+print(f' la resta de {num1} y {num2} es : {res}')
+print(f' la multiplicacion de {num1} y {num2} es : {mul}')
+print(f' la division de {num1} y {num2} es : {div}')
---
+++ 4.py
@@ -0,0 +1,7 @@
+# Base 
+base = input("Ingresar  Base: ")
+altura = input("Ingresa altura: ")
+b = float(base)
+a = float(altura)
+area = (b*a)/2
+print(F'El area del triangulo es : {area}')
