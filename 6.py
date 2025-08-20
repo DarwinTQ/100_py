8795ee65deed3bf33ef80387e1d0bf7db3d4091c
darwin <dturpoqu@unsa.edu.pe>
2025-08-19 22:55:02 -0500

    Day 3 finished

---
+++ 6.py
@@ -0,0 +1,25 @@
+palabra1 = "Hola"
+palabra2 = "mundo"
+espacio = " "
+
+# Unimos las palabras con un espacio en medio
+frase = palabra1 + espacio + palabra2 # Resultado: "Hola mundo"
+print(frase)
+
+risa = "ja"
+carcajada = risa * 5 # Resultado: "jajajajaja"
+print(carcajada)
+
+separador = "-_-" * 10 # Resultado: "-_--_--_--_--_--_--_--_--_--_-"
+print(separador)
+
+#------------------------------------
+name = input("Input your name: ")
+date_born = input("Input your date of birthday: ")
+favorite_animal = input("Input your favorite animal: ")
+dev = 'dev'
+simb ='*'
+print(name+date_born+dev)
+print(favorite_animal+3*simb)
+
+
