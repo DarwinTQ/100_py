641e27e8f92611bd9ae2310f24758a5defa1b83b
darwin <dturpoqu@unsa.edu.pe>
2025-08-26 23:12:36 -0500

    dia nnn para corregir

---
+++ 11.py
@@ -0,0 +1,14 @@
+temperatura = 32
+
+# La condición es: temperatura > 30
+# Como 32 es mayor que 30, la condición es True y el print se ejecuta.
+if temperatura > 30:
+    print("¡Hace calor! No olvides usar protector solar.")
+
+print("Este mensaje se muestra siempre, sin importar la temperatura.")
+
+##-------------------------------------
+calificacion = int(input("Ingresa tu calificación (0-100): "))
+
+if calificacion >= 60:
+    print("¡Felicidades! Has aprobado el curso.")
