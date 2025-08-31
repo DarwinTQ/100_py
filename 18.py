55a5bd5a1f19c218003059bc5fa5ef919ecf97a0
darwin <dturpoqu@unsa.edu.pe>
2025-08-30 23:37:54 -0500

    add new excercise for solve

---
+++ 18.py
@@ -0,0 +1,8 @@
+# code 18
+def count_consonants(s):
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    count = 0
+    for char in s:
+        if char in consonants:
+            count += 1
+    return count
