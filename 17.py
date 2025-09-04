b2f48db3d3a7b04dee99e84622ff80a713ff9b54
darwin <dturpoqu@unsa.edu.pe>
2025-09-03 23:49:14 -0500

    solve 1 excercise

--- 17.py
+++ 17.py
@@ -1,8 +1,10 @@
 # code 17
-def count_vowels(s):
-    vowels = "aeiouAEIOU"
-    count = 0
-    for char in s:
-        if char in vowels:
-            count += 1
-    return count
+# Vamos a imprimir solo los números impares del 1 al 10
+for numero in range(1, 11):
+    # Si el número es par...
+    if numero % 2 == 0:
+        # ...saltamos esta vuelta y vamos a la siguiente.
+        continue
+    
+    # Esta línea solo se ejecuta si el 'continue' no se activó (es decir, si el número es impar).
+    print(numero)
