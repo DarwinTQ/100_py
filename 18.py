a4cc52514dd75a63b2e10ad7f040f4da5a4450ee
darwin <dturpoqu@unsa.edu.pe>
2025-09-04 23:39:31 -0500

    Se cambiaron algunas cosas

--- 18.py
+++ 18.py
@@ -1,8 +1,31 @@
 # code 18
-def count_consonants(s):
-    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
-    count = 0
-    for char in s:
-        if char in consonants:
-            count += 1
-    return count
+# Busquemos si existe una letra 'Z' en la palabra "Python"
+palabra = "Python"
+for letra in palabra:
+    if letra == 'Z':
+        print("¡La letra 'Z' fue encontrada!")
+        break
+else:
+    # Este bloque se ejecuta porque el 'for' terminó todas sus vueltas sin encontrar una 'Z' y sin un 'break'.
+    print("La letra 'Z' no está en la palabra.")
+    #################
+# code 18
+# Busquemos si existe una letra 'Z' en la palabra "Python"
+palabra = "Python"
+for letra in palabra:
+    if letra == 'Z':
+        print("¡La letra 'Z' fue encontrada!")
+        break
+else:
+    # Este bloque se ejecuta porque el 'for' terminó todas sus vueltas sin encontrar una 'Z' y sin un 'break'.
+    print("La letra 'Z' no está en la palabra.")
+# code 18
+# Busquemos si existe una letra 'Z' en la palabra "Python"
+palabra = "Python"
+for letra in palabra:
+    if letra == 'Z':
+        print("¡La letra 'Z' fue encontrada!")
+        break
+else:
+    # Este bloque se ejecuta porque el 'for' terminó todas sus vueltas sin encontrar una 'Z' y sin un 'break'.
+    print("La letra 'Z' no está en la palabra.")
---
+++ 19.py
@@ -0,0 +1,20 @@
+# code 18
+# Busquemos si existe una letra 'Z' en la palabra "Python"
+palabra = "Python"
+for letra in palabra:
+    if letra == 'Z':
+        print("¡La letra 'Z' fue encontrada!")
+        break
+else:
+    # Este bloque se ejecuta porque el 'for' terminó todas sus vueltas sin encontrar una 'Z' y sin un 'break'.
+    print("La letra 'Z' no está en la palabra.")
+# code 18
+# Busquemos si existe una letra 'Z' en la palabra "Python"
+palabra = "Python"
+for letra in palabra:
+    if letra == 'Z':
+        print("¡La letra 'Z' fue encontrada!")
+        break
+else:
+    # Este bloque se ejecuta porque el 'for' terminó todas sus vueltas sin encontrar una 'Z' y sin un 'break'.
+    print("La letra 'Z' no está en la palabra.")
